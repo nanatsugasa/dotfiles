1059d15354f30039e27fe9c16ac935f6d5acdad1
nanatsugasa <139064296+nanatsugasa@users.noreply.github.com>
2026-06-11 23:10:13 +0900

    refactor(Neovim): vim.opt系をinit.luaからlazy.luaに移動

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,21 +1,5 @@
--- 行番号の表示
-vim.opt.number = true
-
--- ==============================
--- インデント設定
--- ==============================
--- タブをスペースに変換する
-vim.opt.expandtab = true
--- タブ文字を4文字分で見せる（既存ファイル用）
-vim.opt.tabstop = 4
--- 自動インデントや「>>」での移動幅
-vim.opt.shiftwidth = 4
--- Tabキーを押したときに入るスペースの数
-vim.opt.softtabstop = 4
-
-
 -- lazey.nvimの読み込み
---require("config.lazy")
+require("config.lazy")
 
 
 --vim.cmd("colorscheme nightfox")
